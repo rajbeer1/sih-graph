46028231f4fb254dc7e3cfd2bc2b6e6333921aa3
loki <uddeeptaraajkashyap@gmail.com>
2023-12-21 18:11:14 +0530

    update

--- src/ResultPage.jsx
+++ src/ResultPage.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import './ResultsPage.css'; // Import the CSS file
+import { FaPercentage, FaBalanceScale, FaWater, FaSeedling } from 'react-icons/fa';
 
 const resultData = {
     'Iron Feed Percentage': 55.17,
     'Silica Feed Percentage': 14.35,
     'Ore Pulp PH': 9.48686,
     'Percentage Silica Concentrate': 4.90,
+};
+
+const icons = {
+    'Iron Feed Percentage': <FaSeedling />,
+    'Silica Feed Percentage': <FaBalanceScale />,
+    'Ore Pulp PH': <FaWater />,
+    'Percentage Silica Concentrate': <FaPercentage />
 };
 
 const imageUri = 'http://res.cloudinary.com/ds90zherj/image/upload/v1703162022/q4unfqssb7yysqh5pc3m.jpg';
@@ -18,7 +26,7 @@ const ResultPage = () => {
             </div>
             <div className="data-container">
                 {Object.entries(resultData).map(([key, value]) => (
-                    <p key={key}><strong>{key}:</strong> {value}</p>
+                    <p key={key}><span className="icon">{icons[key]}</span><strong>{key}:</strong> {value}</p>
                 ))}
             </div>
         </div>
